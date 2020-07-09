a0e0575396330da9dbd71e8221f17aea9d76eae2
Sven Koschnicke <s.koschnicke@gfxpro.com>
2020-07-09 10:06:12 +0200

    feat(ui): show all four undeployed pieces list

--- src/main/kotlin/sc/gui/GameView.kt
+++ src/main/kotlin/sc/gui/GameView.kt
@@ -1,45 +1,42 @@
 package sc.gui
 
 import javafx.beans.property.SimpleStringProperty
-import javafx.scene.control.Button
-import javafx.scene.input.ClipboardContent
-import javafx.scene.input.TransferMode
-import javafx.scene.paint.Color
+import sc.gui.controller.ClientController
 import sc.gui.model.UndeployedPiecesModel
 import sc.gui.view.BoardView
 import sc.gui.view.PiecesFragment
 import sc.plugin2021.PlayerColor
-import tornadofx.*
+import tornadofx.View
+import tornadofx.button
+import tornadofx.hbox
+import tornadofx.vbox
 
 class GameView: View() {
     val input = SimpleStringProperty()
     private val boardView: BoardView by inject()
     val redUndeployedPieces = PiecesFragment(UndeployedPiecesModel(PlayerColor.RED))
     val blueUndeployedPieces = PiecesFragment(UndeployedPiecesModel(PlayerColor.BLUE))
+    val yellowUndeployedPieces = PiecesFragment(UndeployedPiecesModel(PlayerColor.YELLOW))
+    val greenUndeployedPieces = PiecesFragment(UndeployedPiecesModel(PlayerColor.GREEN))
     override val root = vbox {
-        rectangle {
-            width = 100.0
-            height = 100.0
-            fill = Color.BLUE
-            setOnDragDetected {
-                val db = this.startDragAndDrop(TransferMode.MOVE)
-                val content = ClipboardContent()
-                content.putString("content")
-                db.setContent(content)
-                println("Dragging started!")
-                it.consume()
+        hbox {
+            vbox {
+                add(redUndeployedPieces)
+                add(blueUndeployedPieces)
             }
-            setOnDragDone {
-                println("Dragging ended!")
-                it.consume()
+            add(boardView)
+            vbox {
+                add(yellowUndeployedPieces)
+                add(greenUndeployedPieces)
             }
         }
-        children.filterIsInstance<Button>().addClass(AppStyle.tackyButton)
-        vbox {
-            addClass(AppStyle.area2)
-            add(redUndeployedPieces)
-            add(blueUndeployedPieces)
-            add(boardView)
+        hbox {
+            button {
+                text = "Start Client"
+                setOnMouseClicked {
+                    clientController.startClient()
+                }
+            }
         }
     }
 }
--- src/main/kotlin/sc/gui/view/PiecesFragment.kt
+++ src/main/kotlin/sc/gui/view/PiecesFragment.kt
@@ -1,15 +1,10 @@
 package sc.gui.view
 
 import javafx.scene.control.SelectionMode
-import javafx.scene.image.Image
-import javafx.scene.paint.Color
-import sc.gui.AppStyle
-import sc.plugin2021.Piece
 import sc.gui.model.UndeployedPiecesModel
-import sc.plugin2021.PieceShape
+import sc.plugin2021.Piece
 import sc.plugin2021.PlayerColor
 import tornadofx.*
-import tornadofx.Stylesheet.Companion.datagridCell
 
 class PiecesFragment(model: UndeployedPiecesModel): Fragment() {
     override val root = listview(model.undeployedPieces) {
@@ -23,11 +18,11 @@ class PiecesFragment(model: UndeployedPiecesModel): Fragment() {
                 PlayerColor.BLUE -> "blue"
             }
             val filename = "tiles_${color}_$number.png"
-            graphic = stackpane {
+            graphic = hbox {
                 imageview("file:resources/graphics/blokus/$color/$filename") {
                     isSmooth = false
                 }
-                label(filename)
+                label(number)
             }
         }
         onUserSelect {
@@ -35,13 +30,7 @@ class PiecesFragment(model: UndeployedPiecesModel): Fragment() {
         }
     }
 
-    init {
-    }
-
     fun selectedItem(): Piece? {
         return root.selectedItem
-    }
-
-    fun selectOne() {
     }
 }
