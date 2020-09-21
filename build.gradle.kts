b65c2ad08aae0b16c1c1c4bd87d643e932159d27
Sven Koschnicke <s.koschnicke@gfxpro.com>
2020-09-21 11:14:43 +0200

    chore: prepare release

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 group = "sc.gui"
-version = "21.0.0-pre"
+version = "21.0.1"
 try {
 	// Add hash suffix if git is available
 	version = version.toString() + "-" + Runtime.getRuntime().exec(arrayOf("git", "rev-parse", "--short", "--verify", "HEAD")).inputStream.reader().readText().trim()
