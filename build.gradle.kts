7ec4e7b132f27171046b58becfaafdceddb50946
Sven Koschnicke <s.koschnicke@gfxpro.com>
2020-10-07 11:36:01 +0200

    chore(gradle): update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 group = "sc.gui"
-version = "21.0.3"
+version = "21.0.6"
 try {
 	// Add hash suffix if git is available
 	version = version.toString() + "-" + Runtime.getRuntime().exec(arrayOf("git", "rev-parse", "--short", "--verify", "HEAD")).inputStream.reader().readText().trim()
