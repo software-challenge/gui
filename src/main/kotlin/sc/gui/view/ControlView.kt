3673ea9821fbe4bb53a34590e0650b9bd9592348
Aaron Alef <aaron.alef@code.berlin>
2020-10-22 10:39:27 +0200

    feat(ui): add skip button (#41)
    Co-authored-by: Sven Koschnicke <s.koschnicke@gfxpro.com>

--- src/main/kotlin/sc/gui/view/ControlView.kt
+++ src/main/kotlin/sc/gui/view/ControlView.kt
@@ -5,11 +5,9 @@ import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.control.Button
 import sc.gui.AppStyle
-import sc.gui.controller.AppController
-import sc.gui.controller.ClientController
-import sc.gui.controller.GameController
-import sc.gui.controller.GameCreationController
+import sc.gui.controller.*
 import sc.plugin2021.Color
+import sc.plugin2021.SkipMove
 import tornadofx.*
 
 val Color.borderStyle
@@ -28,29 +26,46 @@ class ControlView : View() {
     private val playPauseButton: Button = button {
         text = "Start"
     }
-    private val selected = hbox {
-        visibleProperty().bind(gameCreationController.hasHumanPlayer)
-        addClass(AppStyle.pieceUnselectable)
-        label("Auswahl: ")
-        hbox {
-            addClass(AppStyle.undeployedPiece, gameController.selectedColor.value.borderStyle)
-            gameController.selectedColor.addListener { _, old, new ->
-                if(old != null)
-                    removeClass(old.borderStyle)
-                if(new != null)
-                    addClass(new.borderStyle)
-            }
-            this += PiecesFragment(gameController.selectedColor, gameController.selectedShape, gameController.selectedRotation, gameController.selectedFlip)
-        }
-    }
 
-    override val root = borderpane {
-        padding = Insets(10.0, 0.0, 10.0, 0.0)
-        left = selected
-        center {
+    override val root = hbox {
+        spacing = 8.0
+        hbox {
+            spacing = 8.0
+            visibleProperty().bind(gameCreationController.hasHumanPlayer)
+            addClass(AppStyle.pieceUnselectable)
+            gameController.isHumanTurnProperty().addListener { _, _, humanTurn ->
+                                                                   if (humanTurn) {
+                                                                       removeClass(AppStyle.pieceUnselectable)
+                                                                   } else if (!humanTurn && !hasClass(AppStyle.pieceUnselectable)) {
+                                                                       addClass(AppStyle.pieceUnselectable)
+                                                                   }
+            }
+            label("Auswahl: ")
+            pane {
+                addClass(AppStyle.undeployedPiece, gameController.selectedColor.value.borderStyle)
+                gameController.selectedColor.addListener { _, old, new ->
+                                                               if(old != null)
+                                                           removeClass(old.borderStyle)
+                                                           if(new != null)
+                                                           addClass(new.borderStyle)
+                }
+                this += PiecesFragment(gameController.selectedColor, gameController.selectedShape, gameController.selectedRotation, gameController.selectedFlip)
+            }
+        }
+        vbox {
+            spacing = 8.0
             hbox {
                 alignment = Pos.TOP_CENTER
-                spacing = 10.0
+                button {
+                    enableWhen(gameController.canSkipProperty())
+                    text = "Passen"
+                    setOnMouseClicked {
+                        fire(HumanMoveAction(SkipMove(gameController.currentColorProperty().get())))
+                    }
+                }
+            }
+            hbox {
+                spacing = 8.0
                 this += playPauseButton
                 button {
                     disableWhen(gameController.currentTurnProperty().isEqualTo(0))
@@ -74,12 +89,12 @@ class ControlView : View() {
     }
 
     init {
-		val updatePauseState = { start: Boolean ->
-            if(clientController.controllingClient?.game?.isPaused == true) {
-                playPauseButton.text = if(start) "Start" else "Weiter"
-            } else {
-                playPauseButton.text = "Anhalten"
-            }
+        val updatePauseState = { start: Boolean ->
+                                     if(clientController.controllingClient?.game?.isPaused == true) {
+                                         playPauseButton.text = if(start) "Start" else "Weiter"
+                                     } else {
+                                         playPauseButton.text = "Anhalten"
+                                     }
         }
         playPauseButton.setOnMouseClicked {
             if (gameController.gameEndedProperty().get()) {
@@ -87,25 +102,18 @@ class ControlView : View() {
                 gameController.clearGame()
             } else {
                 clientController.togglePause()
-				updatePauseState(false)
+                updatePauseState(false)
             }
         }
-    
+
         // When the game is paused externally e.g. when rewinding
         gameController.currentTurnProperty().addListener { _, _, turn ->
-            updatePauseState(turn == 0)
+                                                               updatePauseState(turn == 0)
         }
         gameController.gameEndedProperty().addListener { _, _, ended ->
-            if (ended) {
-                playPauseButton.text = "Spiel beenden"
-            }
-        }
-        gameController.isHumanTurnProperty().addListener { _, _, humanTurn ->
-            if (humanTurn) {
-                selected.removeClass(AppStyle.pieceUnselectable)
-            } else if (!humanTurn && !selected.hasClass(AppStyle.pieceUnselectable)) {
-                selected.addClass(AppStyle.pieceUnselectable)
-            }
+                                                             if (ended) {
+                                                                 playPauseButton.text = "Spiel beenden"
+                                                             }
         }
     }
 }
