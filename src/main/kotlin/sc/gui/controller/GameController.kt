72cb2b5612d993852b2c52e9e93cc97355cf9248
Aaron Alef <aaron.alef@code.berlin>
2020-10-22 12:38:11 +0200

    refactor(controller): remove code used for debugging

--- src/main/kotlin/sc/gui/controller/GameController.kt
+++ src/main/kotlin/sc/gui/controller/GameController.kt
@@ -190,17 +190,6 @@ class GameController : Controller() {
             gameState = event.gameState
             canSkipProperty().set(false)
 
-            /* TODO: remove */
-            println("""
-                ========================================================
-                $gameState
-                
-                ${gameState.board}
-                ========================================================
-            """.trimIndent())
-            /**/
-            throw (InvalidMoveException("the hell?"))
-
             // I don't know why orderedColors becomes an empty array and results in CurrentColor being inaccessible (throwing error) when the game ended,
             // but this is how we can avoid it for now TODO("fix this in the plugin")
             if (event.gameState.orderedColors.isNotEmpty()) {
