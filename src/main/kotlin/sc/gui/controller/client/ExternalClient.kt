473fbbfd9b0a23f1fe8a376976116e41aff1cc66
xerus2000 <27jf@pm.me>
2020-12-11 10:49:14 +0100

    docs(clients): add descriptive toString methods

--- src/main/kotlin/sc/gui/controller/client/ExternalClient.kt
+++ src/main/kotlin/sc/gui/controller/client/ExternalClient.kt
@@ -12,4 +12,6 @@ class ExternalClient(private val host: String, private val port: Int) : ClientIn
     override fun joinPreparedGame(reservation: String) {
         throw Exception("External/Manual client can't join a prepared game (reservation: $reservation)")
     }
+    
+    override fun toString() = super.toString() + " on $host:$port"
 }
