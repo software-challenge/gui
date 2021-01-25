2403b43cd31c6dbedd3ed508c53cf32718345b54
xerus2000 <27jf@pm.me>
2021-01-25 12:31:06 +0100

    style(gradle): reformat buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,26 @@ val versionNumber = "21.2.1"
 
 val minJavaVersion = JavaVersion.VERSION_11
 plugins {
-	val minJavaVersion = JavaVersion.VERSION_11 // Declared twice because plugins block has its own scope
-	require(JavaVersion.current() >= minJavaVersion) {
-		"Building requires at least JDK $minJavaVersion - please look into the README."
-	}
-	
-	application
-	kotlin("jvm") version "1.4.20"
-	id("org.openjfx.javafxplugin") version "0.0.9"
-	id("com.github.johnrengelman.shadow") version "6.1.0"
-	
-	id("com.github.ben-manes.versions") version "0.33.0"
-	id("se.patrikerdes.use-latest-versions") version "0.2.15"
+    val minJavaVersion = JavaVersion.VERSION_11 // Declared twice because plugins block has its own scope
+    require(JavaVersion.current() >= minJavaVersion) {
+        "Building requires at least JDK $minJavaVersion - please look into the README"
+    }
+    
+    application
+    kotlin("jvm") version "1.4.20"
+    id("org.openjfx.javafxplugin") version "0.0.9"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    
+    id("com.github.ben-manes.versions") version "0.33.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.15"
 }
 
 group = "sc.gui"
 version = versionNumber
 try {
-	// Add hash suffix if git is available
-	version = version.toString() + "-" + Runtime.getRuntime().exec(arrayOf("git", "rev-parse", "--short", "--verify", "HEAD")).inputStream.reader().readText().trim()
-} catch(_: java.io.IOException) {
+    // Add hash suffix if git is available
+    version = "$version-" + Runtime.getRuntime().exec(arrayOf("git", "rev-parse", "--short", "--verify", "HEAD")).inputStream.reader().readText().trim()
+} catch (_: java.io.IOException) {
 }
 
 application {
@@ -39,70 +39,67 @@ application {
         // For accessing InputMap used in RangeSliderBehavior
         "--add-exports=javafx.controls/com.sun.javafx.scene.control.inputmap=ALL-UNNAMED"
     )
-
 }
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 val backend = gradle.includedBuilds.last()
 
 dependencies {
-	implementation(kotlin("stdlib-jdk8"))
-	
-	implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+    
+    implementation(kotlin("reflect"))
     implementation("no.tornado", "tornadofx", "2.0.0-SNAPSHOT") { exclude("org.jetbrains.kotlin", "kotlin-reflect") }
-	implementation("io.github.microutils", "kotlin-logging-jvm", "2.0.3")
+    implementation("io.github.microutils", "kotlin-logging-jvm", "2.0.3")
     
     implementation(fileTree(backend.name + "/server/build/runnable") { include("**/*.jar") })
 }
 
 tasks {
-	compileJava {
-		options.release.set(minJavaVersion.majorVersion.toInt())
-	}
-	processResources {
-		doFirst{
-			sourceSets.main.get().resources.srcDirs.first().resolve("version.txt").writeText(version.toString())
-		}
-	}
-	withType<KotlinCompile> {
-		dependsOn(backend.task(":server:deploy"))
-		kotlinOptions{
-			jvmTarget = minJavaVersion.toString()
-			freeCompilerArgs = listOf("-Xjvm-default=all")
-		}
-	}
-	
-	javafx {
-		version = "13"
-		modules("javafx.controls", "javafx.fxml", "javafx.base", "javafx.graphics")
-	}
-	
-	shadowJar {
-		destinationDirectory.set(buildDir)
-		archiveClassifier.set(OperatingSystem.current().familyName)
-	}
-	
-	run.configure {
-		workingDir(buildDir.resolve("tmp"))
+    compileJava {
+        options.release.set(minJavaVersion.majorVersion.toInt())
+    }
+    processResources {
         doFirst {
-        	workingDir.mkdirs()
-		}
-	}
-	
-	val release by creating {
-		dependsOn(check)
-		group = "distribution"
-		description = "Creates and pushes a tagged commit with the current version"
-		doLast {
-			exec { commandLine("git", "commit", "-a", "-m", "release: $versionNumber") }
-			exec { commandLine("git", "tag", versionNumber) }
-			exec { commandLine("git", "push", "origin", versionNumber, "master") }
-		}
-	}
+            sourceSets.main.get().resources.srcDirs.single().resolve("version.txt").writeText(version.toString())
+        }
+    }
+    withType<KotlinCompile> {
+        dependsOn(backend.task(":server:deploy"))
+        kotlinOptions {
+            jvmTarget = minJavaVersion.toString()
+            freeCompilerArgs = listOf("-Xjvm-default=all")
+        }
+    }
+    
+    javafx {
+        version = "13"
+        modules("javafx.controls", "javafx.fxml", "javafx.base", "javafx.graphics")
+    }
+    
+    shadowJar {
+        destinationDirectory.set(buildDir)
+        archiveClassifier.set(OperatingSystem.current().familyName)
+    }
+    
+    run.configure {
+        workingDir(buildDir.resolve("tmp"))
+        doFirst {
+            workingDir.mkdirs()
+        }
+    }
+    
+    val release by creating {
+        dependsOn(check)
+        group = "distribution"
+        description = "Creates and pushes a tagged commit with the current version"
+        doLast {
+            exec { commandLine("git", "commit", "-a", "-m", "release: $versionNumber") }
+            exec { commandLine("git", "tag", versionNumber) }
+            exec { commandLine("git", "push", "origin", versionNumber, "master") }
+        }
+    }
 }
