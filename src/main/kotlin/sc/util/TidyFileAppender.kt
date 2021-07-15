2ffe3a8e2a9b8815b9290d4084b2ac0702382ff4
xeruf <27jf@pm.me>
2021-07-15 17:29:44 +0200

    feat: tidy up logging
    - one file per run
    - TidyFileAppender takes care of cleaning up old files
    
    See also: https://stackoverflow.com/a/68393619/6723250

---
+++ src/main/kotlin/sc/util/TidyFileAppender.kt
@@ -0,0 +1,28 @@
+package sc.util
+
+import ch.qos.logback.core.FileAppender
+import java.io.File
+
+class TidyFileAppender<E>: FileAppender<E>() {
+    /** Defaults to the parent dir of the current logfile. */
+    var directory: String? = null
+    /** How many previous files to keep, by last modified attribute. */
+    var maxHistory: Int = 20
+    /** Threshold for extra files that may be kept to reduce file system accesses. */
+    var threshold: Int = 0
+    
+    override fun start() {
+        if (directory == null)
+            directory = File(fileName).parent
+        File(directory).list()?.let { files ->
+            if (files.size > maxHistory + threshold) {
+                files.map { File(directory, it) }
+                        .sortedBy { it.lastModified() }
+                        .take(files.size - maxHistory)
+                        //.also { println("Removing $it") }
+                        .forEach(File::delete)
+            }
+        }
+        super.start()
+    }
+}
