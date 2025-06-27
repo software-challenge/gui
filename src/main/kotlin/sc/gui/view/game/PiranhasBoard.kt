dc27e8ebcf88cfdd9f678345af51b2c24cf36b5c
tj <git@mosasini.de>
2025-06-27 10:34:48 +0200

    feat(plugin26): GameRuleLogic targetField calculation

--- src/main/kotlin/sc/gui/view/game/PiranhasBoard.kt
+++ src/main/kotlin/sc/gui/view/game/PiranhasBoard.kt
@@ -1,6 +1,8 @@
 package sc.gui.view.game
 
 import javafx.geometry.Pos
+import javafx.scene.Node
+import javafx.scene.effect.ColorAdjust
 import javafx.scene.input.KeyEvent
 import javafx.scene.layout.GridPane
 import sc.api.plugins.Coordinates
@@ -8,6 +10,7 @@ import sc.gui.view.GameBoard
 import sc.gui.view.PieceImage
 import sc.plugin2026.FieldState
 import sc.plugin2026.GameState
+import sc.plugin2026.util.GameRuleLogic
 import sc.plugin2026.util.PiranhaConstants
 import tornadofx.*
 
@@ -23,11 +26,24 @@ class PiranhasBoard: GameBoard<GameState>() {
         add(grid)
     }
     
+    val hovers = ArrayList<Node>()
+    
     override fun onNewState(oldState: GameState?, state: GameState?) {
         grid.children.clear()
         state?.let { state ->
             state.board.forEach { (pos: Coordinates, field: FieldState) ->
-                val piece = PieceImage(gridSize, field.team?.let { team -> "${team}_${field.size}" } ?: field.name.lowercase())
+                val piece = PieceImage(gridSize,
+                    field.team?.let { team -> "${team}_${field.size}" } ?: field.name.lowercase())
+                if(field.team != null)
+                piece.onLeftClick {
+                    GameRuleLogic.possibleMovesFor(state.board, pos).forEach { move ->
+                        val target = GameRuleLogic.targetField(state.board, move)
+                        val hover = PieceImage(gridSize, "${field.team}_${field.size}")
+                        hover.effect = ColorAdjust().apply { saturation = -0.5 }
+                        hovers.add(hover)
+                        grid.add(hover, target.x, target.y)
+                    }
+                }
                 grid.add(piece, pos.x, pos.y)
             }
         }
